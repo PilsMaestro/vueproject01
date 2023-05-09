08f89ac55539b8bd1169eb260e36ca4ef9c67bd7
Nils Rosier <nils_rosier@hotmail.com>
2023-05-09 20:21:36 +0200

    Update 9/5/23

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,123 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <header class="headerHome">
+    <div>
+      <img class="logo" src="../assets/Borrow4Less_logo.png" alt="logoBorrow4less">
+    </div>
+    <nav class="navHome">
+      <div class="navItem" :key="idx" v-for="(navItem, idx) in navColumns">
+        <h1> {{ navItem }}
+        </h1>
+      </div>
+    </nav>
+  </header>
+  <div class="container">
+    <img src="../assets/img3.png" class="image" alt="Image">
+    <div class="text">Your Property Leading Club</div>
+    <div class="text2">Lend on property loans and earn 7-12% return p.a.</div>
+  </div>
+
+  <div class="TitelVet"> Looking for real returns?
+  </div>
+  <img src="../assets/img2.png" class="kleinefoto" alt="foto">
+  <div class="textondertitel">
+    Lend from €1.000 and earn up to 12% fixed return p.a. on property loans through Blend.
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
+const navColumns = [
+  'Home',
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+  'About',
+
+  'Contact',
+
+  'Login',
+
+  'Register',
+];
+
 </script>
+
+<style scoped>
+.headerHome {
+  display: flex;
+  justify-content: space-between;
+  background-color: rgb(0, 16, 88);
+  color: rgb(206, 206, 206);
+  align-items: center;
+}
+
+.clickable-header {
+  cursor: pointer;
+  text-decoration: underline;
+  color: rgb(97, 97, 97);
+}
+
+.navHome {
+  display: flex;
+}
+
+.navItem {
+  margin: 10px;
+}
+
+.logo {
+
+  width: 60px;
+
+  padding: 10px;
+
+  padding-left: 20px;
+
+}
+
+.container {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+  filter: brightness(50%);
+}
+
+.text {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 40px;
+  font-weight: bold;
+}
+
+.text2 {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.TitelVet {
+  font-weight: bold;
+  font-size: 20px;
+  position: center;
+}
+
+body {
+  background-color: #4ce5e0;
+}
+
+.kleinefoto {
+  width: 30%;
+  height: auto;
+  float: right;
+  border-radius: 5%;
+  margin-right: 50px;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  mode: "jit",
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      mob: "450px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      body: ["Nunito Sans"],
+    },
+    fontSize: {
+      xxs: ".55rem",
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+    },
+    colors: {
+      blue: {
+        light: "#ECECF4",
+        ligth10: "rgba(213,218,238,0.1)",
+        default60: "rgba(25, 38, 92, 0.6)",
+        default50: "rgb(140 147 174)",
+        default40: "rgba(25, 38, 92, 0.4)",
+        default30: "#bfc1d0",
+        default10: "rgb(232 234 239)",
+        default07: "rgba(25, 38, 92, 0.07)",
+        500: "#3a5194",
+        DEFAULT: "#19265C",
+      },
+      white: {
+        DEFAULT: "#FFFFFF",
+        100: "#FAFAFA",
+      },
+      turquoise: {
+        light: "rgba(116, 215, 244, 0.15)",
+        DEFAULT: "#74D7F4",
+      },
+      purple: {
+        DEFAULT: "#8380FF",
+        default15: "rgba(131, 128, 255, 0.15)",
+        default70: "rgba(131, 128, 255, 0.7)",
+      },
+      black: {
+        DEFAULT: "#0E1724",
+      },
+      red: {
+        DEFAULT: "#FF6666",
+        default30: "rgba(255, 102, 102, 0.5)",
+      },
+      yellow: {
+        DEFAULT: "#FFAA00",
+        default25: "rgb(255, 170, 0, 0.25)",
+      },
+      green: {
+        DEFAULT: "#228C22",
+      },
+      gray: {
+        DEFAULT: "#808080",
+        default40: "rgba(128, 128, 128, 0.4)",
+      },
+    },
+    extend: {
+      height: {
+        "75-screen": "75vh",
+      },
+    },
+  },
+  plugins: [],
+  safelist: ["text-gray", "text-yellow", "text-red", "bg-gray", "bg-red", "bg-yellow", "bg-blue"],
+};
